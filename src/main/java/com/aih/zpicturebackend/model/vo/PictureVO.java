01c88a4ea86a153a1c89b8623e9af2e33e549641
AiH_Madam <2474905733@qq.com>
2026-04-07 00:57:57 +0800

    feat: 完善图片审核处理

--- src/main/java/com/aih/zpicturebackend/model/vo/PictureVO.java
+++ src/main/java/com/aih/zpicturebackend/model/vo/PictureVO.java
@@ -89,6 +89,16 @@ public class PictureVO implements Serializable {
     private String picFormat;
 
     /**
+     * 审核状态：0-待审核; 1-通过; 2-拒绝
+     */
+    private Integer reviewStatus;
+
+    /**
+     * 审核信息
+     */
+    private String reviewMessage;
+
+    /**
      * 用户 id
      */
     private Long userId;
